4b2491ff485b01bf42750553239b32c389dddd02
Davi <davicr38@gmail.com>
2025-01-24 12:05:10 -0300

    Implement CRotDoor `m_flSpeed` fix

--- BloodlinesPatch/VampireGame.h
+++ BloodlinesPatch/VampireGame.h
@@ -1,15 +1,19 @@
 #pragma once
 
+#include <cstdint>
 #include <Windows.h>
 
+#define VAMPIRE_STEAM_DLLINIT_OFFSET 0x11A0C0
 #define VAMPIRE_STEAM_GPGLOBALS_OFFSET 0x70B228
-#define VAMPIRE_STEAM_BASEDOORBLOCKED_OFFSET 0xF14A0
-#define VAMPIRE_STEAM_ROTDOORBLOCKED_OFFSET 0xF3900
 #define VAMPIRE_STEAM_BLOCKEDFRAMES_OFFSET 0xF3A6D
+#define VAMPIRE_STEAM_ANGULARMOVESPEED_OFFSET 0x1C189A
+#define VAMPIRE_STEAM_CROTDOOR_VTABLE_OFFSET 0x454B94
 
 namespace Vampire {
 
 // (vampire.dll)
+uint8_t* VampireBase = nullptr;
+
 struct CGlobalVarsBase {
     float realtime;
     int framecount;
@@ -19,10 +23,9 @@ struct CGlobalVarsBase {
 };
 CGlobalVarsBase* gpGlobals = nullptr;
 
-typedef void (*BaseDoorBlockedFn)(uintptr_t _this, uintptr_t pOther);
-BaseDoorBlockedFn BaseDoorBlocked = nullptr;
-
 // (tier0.dll)
+uint8_t* Tier0Base = nullptr;
+
 typedef void (*MsgFn)(const char* pMsgFormat, ...);
 MsgFn Msg = nullptr;
 
--- BloodlinesPatch/main.cpp
+++ BloodlinesPatch/main.cpp
@@ -1,24 +1,50 @@
+#include <cstdint>
 #include <Windows.h>
 
 #include "VampireGame.h"
-
 #include "safetyhook.hpp"
 
-SafetyHookMid blockedFramesHook {};
-void BlockedFramesHook(SafetyHookContext& ctx) { ctx.ecx = 2; }
+SafetyHookMid g_blockedFramesHook {};
+void BlockedFramesHook(SafetyHookContext& ctx)
+{
+    // Troika's implementation takes a specific path for unblocking doors when it detects they have been blocked by an entity for
+    // more than 15 frames. I've seen no difference with the original behavior yet by disabling this path by making sure the frame
+    // counter is always 2.
+    ctx.ecx = 2;
+}
+
+SafetyHookMid g_toggleAngularMoveHook {};
+void ToggleAngularMoveHook(SafetyHookContext& ctx)
+{
+    // Check if the CBaseToggle is a CRotDoor to make sure we are not affecting any other entities.
+    if (*reinterpret_cast<uint8_t**>(ctx.esi) == Vampire::VampireBase + VAMPIRE_STEAM_CROTDOOR_VTABLE_OFFSET) {
+        // Multiply the `flSpeed` by a framerate-dependent factor in the velocity determination logic inside
+        // CBaseToggle::AngularMove.
+        float* flSpeed = reinterpret_cast<float*>(reinterpret_cast<uint8_t*>(ctx.esp) + 0x38);
+        *flSpeed *= (1.0f / Vampire::gpGlobals->frametime) / 60.0f;
+    }
+}
+
+SafetyHookInline g_dllInitHook {};
+bool DllInitHook(uintptr_t engine, uintptr_t physics, uintptr_t filesystem, Vampire::CGlobalVarsBase* pGlobals)
+{
+    Vampire::gpGlobals = pGlobals;
+    g_dllInitHook.call<bool>(engine, physics, filesystem, pGlobals);
+}
 
 extern "C" __declspec(dllexport) bool loaded_vampire()
 {
     // Initialize Vampire addresses.
-    Vampire::gpGlobals
-        = *(Vampire::CGlobalVarsBase**)(reinterpret_cast<char*>(GetModuleHandleA("vampire.dll")) + VAMPIRE_STEAM_GPGLOBALS_OFFSET);
-    Vampire::BaseDoorBlocked = reinterpret_cast<Vampire::BaseDoorBlockedFn>(
-        reinterpret_cast<char*>(GetModuleHandleA("vampire.dll") + VAMPIRE_STEAM_BASEDOORBLOCKED_OFFSET));
-    Vampire::Msg = reinterpret_cast<Vampire::MsgFn>(GetProcAddress(GetModuleHandleA("tier0.dll"), "Msg"));
+    Vampire::VampireBase = reinterpret_cast<uint8_t*>(GetModuleHandleA("vampire.dll"));
+    Vampire::Tier0Base = reinterpret_cast<uint8_t*>(GetModuleHandleA("tier0.dll"));
+    Vampire::Msg = reinterpret_cast<Vampire::MsgFn>(GetProcAddress(reinterpret_cast<HMODULE>(Vampire::Tier0Base), "Msg"));
+
+    // Grab gpGlobals from DLLInit.
+    g_dllInitHook = safetyhook::create_inline(Vampire::VampireBase + VAMPIRE_STEAM_DLLINIT_OFFSET, DllInitHook);
 
     // Initialize Door FPS Fix.
-    blockedFramesHook = safetyhook::create_mid(
-        reinterpret_cast<char*>(GetModuleHandleA("vampire.dll")) + VAMPIRE_STEAM_BLOCKEDFRAMES_OFFSET, BlockedFramesHook);
+    g_blockedFramesHook = safetyhook::create_mid(Vampire::VampireBase + VAMPIRE_STEAM_BLOCKEDFRAMES_OFFSET, BlockedFramesHook);
+    g_toggleAngularMoveHook = safetyhook::create_mid(Vampire::VampireBase + VAMPIRE_STEAM_ANGULARMOVESPEED_OFFSET, ToggleAngularMoveHook);
 
     return 0;
 }
